890361ae1ead54ff8c6881398c87f3e3c020e6a9
Judy McGoldrick <jjeanius@verizon.net>
2019-01-15 03:23:25 +0000

    Automatically backed up by Learn

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,7 @@ class Passenger < ActiveRecord::Base
   has_many :rides
   has_many :taxis, through: :rides
 
-  @passenger.taxis << taxi
+  def passenger
+    @passenger.taxis << taxi
+    @passenger.rides << ride
 end
