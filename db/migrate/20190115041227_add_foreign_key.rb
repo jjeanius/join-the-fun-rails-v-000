adad33a31071334e9ef39c0e42476a7ef53d1ded
Judy McGoldrick <jjeanius@verizon.net>
2019-01-15 04:22:59 +0000

    Automatically backed up by Learn

--- db/migrate/20190115041227_add_foreign_key.rb
+++ db/migrate/20190115041227_add_foreign_key.rb
@@ -1,14 +1,7 @@
 class AddForeignKey < ActiveRecord::Migration
   def change
-    create_table :rides do |t|
+    add_foreign_key :rides do |t|
       t.belongs_to :passenger
       t.belongs_to :taxi
   end
 end
-create_table :appointments do |t|
-     t.belongs_to :physician, index: true
-     t.belongs_to :patient, index: true
-     t.datetime :appointment_date
-     t.timestamps
-   end
- end
